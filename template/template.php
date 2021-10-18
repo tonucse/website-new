6bcb4951dfb4b36d4709a76b8c097c2927cd5978
tonucse <72280629+tonucse@users.noreply.github.com>
2021-10-18 15:46:02 +0600

    Update template.php

--- template/template.php
+++ template/template.php
@@ -29,3 +29,10 @@
 </div>
 </body>
 </html>
+
+<head>
+<title>My First Webpage</title>
+<meta charset="UTF-8">
+<meta name="description" content="This field contains information about your page. It is usually around two sentences long.">.
+<meta name="author" content="Conor Sheils">
+</header>
